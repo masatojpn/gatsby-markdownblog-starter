902ca5b0eb345e1df2e701a79045f2a8a9ba4235
Masato Nakamura <m.nakamura@madesignartworks.com>
2019-03-20 16:09:32 +0900

    headerのサンプルファイルを追加

---
+++ src/components/exampleheader.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from 'exampleheader.module.scss'
+import styled from 'styled-components'
+
+
+export default () => (
+  <>
+    <Wrapper />
+    <header className={styles.headerClass} />
+  </>
+)
+
+const Wrapper = () => styled.header`
+  display: block;
+  position: fixed;
+  width: 100%;
+  height: 80px;
+  z-index: 10;
+`
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 
 import { rhythm, scale } from "../utils/typography"
+
+// Import Global Style
 import "../styles/style.scss"
+
+// Import Header
+import Header from "../components/exampleheader"
 
 class Layout extends React.Component {
   render() {
@@ -53,22 +58,25 @@ class Layout extends React.Component {
       )
     }
     return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
-        <header>{header}</header>
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <>
+        <Header />
+        <div
+          style={{
+            marginLeft: `auto`,
+            marginRight: `auto`,
+            maxWidth: rhythm(24),
+            padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          }}
+        >
+          <header>{header}</header>
+          <main>{children}</main>
+          <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        </div>
+      </>
     )
   }
 }
