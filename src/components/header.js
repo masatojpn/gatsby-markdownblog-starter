a05cc5e024d6b96e47da0709326a93e3092933c9
Masato Nakamura <m.nakamura@madesignartworks.com>
2019-03-20 11:51:49 +0900

    Initial commit

---
+++ src/components/header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+// import styles from 'header.module.css'
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+      </header>
+    )
+  }
+}
+
+// export default () => (
+//   <Wrapper>
+
+//   </Wrapper>
+// )
+
+// const Wrapper = () => styled.header`
+//   display: block;
+//   position: fixed;
+//   width: 100%;
+//   height: 80px;
+//   z-index: 10;
+// `
